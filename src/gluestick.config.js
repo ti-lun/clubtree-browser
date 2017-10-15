8cacb90af95dcd0f655e919a204a60090555e761
David Dinh <ddinh1@uci.edu>
2017-10-14 17:07:29 -0700

    Change port config to server.

--- src/gluestick.config.js
+++ src/gluestick.config.js
@@ -3,8 +3,8 @@ export default config => ({
   protocol: 'http',
   host: "localhost",
   ports: {
-    client: process.env.PORT || 8888,
-    server: 8880
+    client: 8888,
+    server: process.env.PORT || 8880
   },
   publicPath: process.env.ASSET_URL || "/assets/"
 });
