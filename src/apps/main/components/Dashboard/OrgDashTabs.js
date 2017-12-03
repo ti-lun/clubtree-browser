ffea732fd9c44dd225f32c377b225df30f95f4a3
David Dinh <ddinh1@uci.edu>
2017-12-03 14:42:08 -0800

    Fix dash tabs sometimes appearing in a different order.

--- src/apps/main/components/Dashboard/OrgDashTabs.js
+++ src/apps/main/components/Dashboard/OrgDashTabs.js
@@ -3,6 +3,7 @@
 import React, { Component } from "react";
 import { Link } from "react-router";
 import axios from "axios";
+import Promise from "bluebird";
 
 import { API_URL } from "../../lib/consts";
 
@@ -15,37 +16,39 @@ export default class OrgDashTabs extends Component {
     };
   }
 
-  async componentWillReceiveProps(nextProps) {
+  componentWillReceiveProps(nextProps) {
     // if (nextProps.clubs !== this.props.clubs) {
-      let newTabs = [];
-      let tabIndex = 0;
-      await Promise.all(nextProps.clubs.map(async (club, index) => {
-        if (club.club) {
-          const clubInfo = await axios.get(`${API_URL}/clubs/${club.club}`).then((response) => {
-              tabIndex++;
-              newTabs.push(
-                  <div
-                    onClick={ () => this.props.viewDiffClub(response, club) }
-                    key={newTabs.length}
-                    className={`dash-tabs include: single-dash-tab${newTabs.length} mild-shadow`}>
-                    { response.data.clubName }
-                  </div>
-              );
-            });
-          }
-        }));
-        tabIndex++;
-        newTabs.push(
-          <Link key={newTabs.length} to="/clubcreation">
-            <div className={`dash-tabs include: single-dash-tab${newTabs.length} mild-shadow`}>
-              Add club
+    let newTabs = [];
+    let tabIndex = 0;
+
+    return Promise.mapSeries(nextProps.clubs, (club, index) => {
+      if (club.club) {
+        return axios.get(`${API_URL}/clubs/${club.club}`).then((response) => {
+          tabIndex++;
+          newTabs.push(
+            <div
+              onClick={() => this.props.viewDiffClub(response, club)}
+              key={newTabs.length}
+              className={`dash-tabs include: single-dash-tab${newTabs.length} mild-shadow`}>
+              {response.data.clubName}
             </div>
+          );
+        });
+      }
+    }).then(() => {
+      tabIndex++;
+      newTabs.push(
+        <Link key={newTabs.length} to="/clubcreation">
+          <div className={`dash-tabs include: single-dash-tab${newTabs.length} mild-shadow`}>
+            Add club
+              </div>
         </Link>
       )
 
       this.setState({
         tabs: newTabs
       });
+    });
     // }
 
   }
